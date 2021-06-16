f8525bc17f535bc0e34938c82b3b67b30cbf1b83
mischievousdev <miscdev.py@gmail.com>
2021-06-16 07:07:02 +0000

    Add events.py

---
+++ cogs/events.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        elif isinstance(error, commands.NotOwner):
+            return
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            return await ctx.send(f"{ctx.command.name} requires {error.param.name} argument, you missed giving that! For more information see: `{ctx.prefix}help {ctx.command.name}`")
+        
+        elif isinstance(error, commands.BadArgument):
+            return await ctx.send(f"{''.join(error.args)} not found!")
+
+def setup(bot):
+    bot.add_cog(Events(bot))
